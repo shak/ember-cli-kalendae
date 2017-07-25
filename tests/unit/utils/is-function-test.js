420c61bf403a31e13fa5f56b38fac09db97eb5f8
Shahrukh Omar <shahrukh.dev@gmail.com>
2017-07-25 19:42:40 +0100

    [test] adds test for the util

---
+++ tests/unit/utils/is-function-test.js
@@ -0,0 +1,12 @@
+import isFunction from 'dummy/utils/is-function';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | is function');
+
+test('it works as expected', function(assert) {
+  let result = isFunction(function() {});
+  assert.ok(result);
+
+  result = isFunction(undefined);
+  assert.strictEqual(result, false);
+});
