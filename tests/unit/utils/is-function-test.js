3f3fd0d82096c23af85e83ab8634dae194f75313
Shahrukh Omar <shahrukh.omar@infinity.co>
2018-10-17 09:26:25 +0100

    upgraded babel to latest

--- tests/unit/utils/is-function-test.js
+++ tests/unit/utils/is-function-test.js
@@ -1,12 +1,12 @@
 import isFunction from 'dummy/utils/is-function';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | is function');
+module('Unit | Utility | is function', function() {
+  test('it works as expected', function(assert) {
+    let result = isFunction(function() {});
+    assert.ok(result);
 
-test('it works as expected', function(assert) {
-  let result = isFunction(function() {});
-  assert.ok(result);
-
-  result = isFunction(undefined);
-  assert.strictEqual(result, false);
+    result = isFunction(undefined);
+    assert.strictEqual(result, false);
+  });
 });
